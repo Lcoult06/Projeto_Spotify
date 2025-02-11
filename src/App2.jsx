010b84039df72c9b7e2b95dc6387b8e2018b0daf
Leo Coutinho <leocoutinho194@gmail.com>
2025-02-11 09:24:52 -0300

    first commit-primeira etapa do projeto

---
+++ src/App2.jsx
@@ -0,0 +1,38 @@
+// import React from "react";
+
+// const App2 = () => {
+//     return <div>App2</div>;
+// };
+
+// export default App2;
+
+
+//rafce
+
+//Arrow Function 
+//const App2 = () => <h1>Ola mundo</h1>;
+
+// function App  () {
+//     return (
+//       <>
+//         <h1>Ola mundo</h1>
+//       </>
+//     )
+//   }
+
+//Nomeação de variavel, funcao, ... = camelCase
+
+//Nomeação de componentes = PascalCase
+// export default, posso importar com qualquer nome e sem chaves
+//export 'sem o default', só posso importar dentro de chaves e com o nome exportado
+
+//Self clossing tag = <Header><HEADER> / <Header/>
+
+//Metodologia BEM = BLOCKS, ELEMENTS e MODIFIERS 
+//bloco__elemento--modificador
+//header
+//header__link
+//header__link--small
+//Nomes compostos são separados por - ex: item-list__header
+
+//tag vazia <></> fragmento
